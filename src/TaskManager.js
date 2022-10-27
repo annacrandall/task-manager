c45687dae6f79a5af95692b9841ea90cc8cbdcb0
ana crandall <107437409+annacrandall@users.noreply.github.com>
2022-10-26 21:57:21 -0500

    "Added styling to updateTask div."

--- src/TaskManager.js
+++ src/TaskManager.js
@@ -1,16 +1,17 @@
 import { useState } from "react";
 import Button from "./components/Button";
 import trashIcon from "./trashIcon.png";
+import InputBox from "./components/InputBox";
 
 const TaskManager = () => {
   const [tasks, setTasks] = useState([]);
   const [customTask, setCustomTask] = useState("");
   const [customDescription, setCustomDescription] = useState("");
-  const [editTask, setEditTask] = useState(""); 
-  const [editTaskDescription, setEditTaskDescription] = useState(""); 
-  const [showEdit, setShowEdit] = useState(-1); 
+  const [editTask, setEditTask] = useState("");
+  const [editTaskDescription, setEditTaskDescription] = useState("");
+  const [showEdit, setShowEdit] = useState(-1);
 
-  const addTask = () => {
+  const handleTask = () => {
     const taskDetail = {
       id: tasks.length === 0 ? 1 : tasks.length + 1,
       task: customTask,
@@ -21,6 +22,11 @@ const TaskManager = () => {
     setTasks((oldList) => [...oldList, taskDetail]);
   };
 
+  const handleClearInput = () => {
+    setCustomTask("");
+    setCustomDescription("");
+    console.log("");
+  };
 
   const handleDelete = (idToDelete) => {
     const newArray = tasks.filter((tasks) => {
@@ -30,62 +36,29 @@ const TaskManager = () => {
   };
 
   const handleEdit = (idToEdit, newTaskName, newDescription) => {
-    const currentTask = tasks.filter((item) => item.idToEdit === idToEdit)
+    const currentTask = tasks.filter((item) => item.idToEdit === idToEdit);
     const newTask = {
       id: currentTask.idToEdit,
       task: newTaskName,
       taskDescription: newDescription,
-    }; 
-    handleDelete(idToEdit)
-    setTasks((oldList) => [...oldList, newTask])
-    setEditTask("") 
+    };
+    handleDelete(idToEdit);
+    setTasks((oldList) => [...oldList, newTask]);
+    setEditTask("");
     setEditTaskDescription("");
-    setShowEdit(-1); 
-  }
- 
+    setShowEdit(-1);
+  };
 
   return (
     <div id="viewport-container" className="font-1 m-2 p-1">
       <h1 className="flex justify-center text-2xl">Task Manager</h1>
 
-      <div className="border-2 border-slate-600 m-3 inline-block grid grid-cols-1">
-        <input
-          type="text"
-          placeholder="Add your task..."
-          task={customTask}
-          onChange={(e) => setCustomTask(e.target.value)}
-          className="m-1 px-2 py-1"
-        />
-        <input
-          type="text"
-          placeholder="Input task description..."
-          taskDescription={customDescription}
-          onChange={(e) => setCustomDescription(e.target.value)}
-          className="m-1 px-2 py-1"
-        />
-        <div id="task-urgency" className="flex flex-row">
-          <h3 className="m-1 p-1">Set task urgency: </h3>
-          <button
-            id="low-urgency"
-            className="text-white bg-green-600 opacity-50 px-2 m-1 rounded text-lg"
-          >
-            Low
-          </button>
-          <button
-            id="medium-urgency"
-            className="text-white bg-amber-600 px-2 opacity-50 m-1 rounded text-lg"
-          >
-            Medium
-          </button>
-          <button
-            id="high-urgency"
-            className="text-white bg-red-600 px-2 rounded opacity-50 m-1 text-lg"
-          >
-            High
-          </button>
-        </div>
-        <Button onClick={addTask} name="Save task"/>
-      </div>
+      <InputBox
+        setCustomTask={setCustomTask}
+        setCustomDescription={setCustomDescription}
+        handleTask={handleTask}
+        Button={Button}
+      />
 
       <div>
         <div className="grid grid-cols-2 px-2 py-1 m-1" id="lists-container">
@@ -97,29 +70,57 @@ const TaskManager = () => {
             <ul>
               {tasks.map((taskDetail) => {
                 return (
-                  <div key={taskDetail.id} id="task-container" className="inline-block">
-                    <li className="grid grid-cols-1 border border-slate-600 rounded px-2 py-1 m-2" onClick={() => setShowEdit(taskDetail.id)}>
-
-                      {taskDetail.task} : {taskDetail.taskDescription}
+                  <div key={taskDetail.id} id="task-container">
+                    <li
+                      className="grid grid-cols-1 border border-slate-600 rounded px-2 py-1 m-2"
+                      onClick={() => setShowEdit(taskDetail.id)}
+                    >
+                      {taskDetail.task}: {taskDetail.taskDescription}
                       <button
                         onClick={() => handleDelete(taskDetail.id)}
                         id="delete-button"
-                        className=" w-10 rounded-xl border border-slate-600"
+                        className=" w-7 rounded border border-slate-600"
                       >
                         <img
                           src={trashIcon}
                           alt="trash icon for delete button"
-                          className="w-6 inline-block m-1"
+                          className="w-4 inline-block m-1"
                         />
                       </button>
                     </li>
                     {showEdit === taskDetail.id ? (
-                      <div className="border border-slate-600 m-1 p-1">
-                        <input id="edit-task-name" type="text" value={editTask}
-                        onChange={(e)=> setEditTask(e.target.value)}/>
-                        <input id="edit-task-description" type="text" value={editTaskDescription}
-                          onChange={(e) => setEditTaskDescription(e.target.value)} />
-                        <button onClick={() => handleEdit(taskDetail.id, editTask, editTaskDescription)}>Update Task</button>
+                      <div className="border border-slate-600 m-2 px-2 py-1 rounded">
+                        <h1>Update Task Info: </h1>
+                        <input
+                          id="edit-task-name"
+                          type="text"
+                          value={editTask}
+                          onChange={(e) => setEditTask(e.target.value)}
+                          className="border border-slate-600 rounded m-1" 
+                          placeholder=" Task Name"
+                        />
+                        <input
+                          id="edit-task-description"
+                          type="text"
+                          value={editTaskDescription}
+                          onChange={(e) =>
+                            setEditTaskDescription(e.target.value)
+                          }
+                          placeholder=" Task Description"
+                          className="border border-slate-600 rounded m-1"
+                        />
+                        <button
+                          onClick={() =>
+                            handleEdit(
+                              taskDetail.id,
+                              editTask,
+                              editTaskDescription
+                            )
+                          }
+                          className="border border-slate-600 rounded px-2 m-1 text-center"
+                        >
+                          Submit
+                        </button>
                       </div>
                     ) : null}
                   </div>
